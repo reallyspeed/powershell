30a7c6a649f2d0be30c279a3c314a12aff4b65a4
reallyspeed <david@reallyspeed.com>
2017-03-23 15:27:14 -0500

    Update client23.ps1

--- client23.ps1
+++ client23.ps1
@@ -50,32 +50,40 @@ function StartMessageReceiver ()
 	{
 		function ProcessMessage($type, $stream)
 		{
-			while (!$server_stream.DataAvailable)
+			while (!$server_stream.DataAvailable) {}
+			
+			try
 			{
-				try
-				{
-					switch($type)
-						1 {
-							$byte_stream = New-Object byte[] 2
-							$count = $server_stream.Read($byte_stream, 0, 2) # Blocking
-							write-host "BS: $byte_stream"
-							$x = [BitConverter]::ToInt16($byte_stream, 0)
-							write-host "Value: $x"
-		
-						}
-						5 {
-							
-						}
-						default {
-							[console]::writeline("Unknown message type: $type")
-						}
-						
-				} catch [Exception] {
-					[console]::writeline("Connection Lost (Processing type $type): `n{0}" -f $_.Exception.Message)
-					break
-				}
+				switch($type)
+					1 { #connect
+						$length = New-Object byte[] 2
+						$count = $server_stream.Read($length, 0, 2) 
+						#if count = 0
+						$length = [BitConverter]::ToInt16($length, 0)
+						write-host "message length: $length"
+						write-host "Value: $x"
 
+					}
+					5 { #state
+						$byte_stream = New-Object byte[] 2
+						$count = $server_stream.Read($byte_stream, 0, 2)
+						#if count = 0
+						write-host "BS: $byte_stream"
+						$x = [BitConverter]::ToInt16($byte_stream, 0)
+						write-host "Value: $x"
+						$message = [System.Text.Encoding]::ASCII.GetString($bytes[0..($return - 1)])
+
+					}
+					default {
+						[console]::writeline("Unknown message type: $type")
+					}
+
+			} catch [Exception] {
+				[console]::writeline("Connection Lost (Processing type $type): `n{0}" -f $_.Exception.Message)
+				break
 			}
+
+			
 		}
 		
 		$buff_size = $client.ReceiveBufferSize
@@ -89,16 +97,15 @@ function StartMessageReceiver ()
 					try
 					{
 						[console]::writeline("!!! Data Available")
-						$count = $server_stream.Read($bytes, 0, $buff_size)
+						#$count = $server_stream.Read($bytes, 0, $buff_size)
 						
 						$type = New-Object byte[] 1
 						$count = $server_stream.Read($type, 0, 1) # Blocking
-						write-host "T: $type"
 						
-						ProcessMessage $type $server_stream
+						
 						If ($count -gt 0)
 						{
-							$message = [System.Text.Encoding]::ASCII.GetString($bytes[0..($return - 1)])
+							ProcessMessage $type $server_stream
 						} Else {
 							[console]::writeline("!!! Read Timeout? Lost connection")
 							Break
@@ -125,11 +132,9 @@ function StartMessageReceiver ()
 	
 	$runspace_message_receive = [RunSpaceFactory]::CreateRunspace()
 	$runspace_message_receive.Open()
-	$runspace_message_receive.SessionStateProxy.setVariable("clients", $clients)
-	$runspace_message_receive.SessionStateProxy.setVariable("remove_client_queue", $remove_client_queue)
-	$runspace_message_receive.SessionStateProxy.setVariable("client_message_queue", $client_message_queue)
+	$runspace_message_receive.SessionStateProxy.setVariable("clients", $client)
+	$runspace_message_receive.SessionStateProxy.setVariable("remove_client_queue", $server_stream)
 	$runspace_message_receive.SessionStateProxy.setVariable("server_state", $server_state)
-	$runspace_message_receive.SessionStateProxy.setVariable("username", $username)
 	$shell_message_receive = [PowerShell]::Create()
 	$shell_message_receive.Runspace = $runspace_message_receive
 	$job = "" | select job, shell
