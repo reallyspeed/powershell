90224c1ff6bd1185726676508122586a0c23fc2e
reallyspeed <david@reallyspeed.com>
2017-03-26 23:51:52 -0500

    Add files via upload

--- client.ps1
+++ client.ps1
@@ -1,17 +1,95 @@
-﻿[CmdletBinding()]
-
+[CmdletBinding()]
+#Param(
+#$Command = $(Read-Host "Enter the script file"), 
+#[Parameter(ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)]$ObjectList
+#$(Read-Host "Enter the script file")
 param(
 	[string]$serverip="127.0.0.1",
-	[int]$port=15608
+	[int]$port=15608,
+	#[Parameter(Mandatory=$true)]
+	[string]$username="u1"
 )
 
-function send-msg ($message, $port, $server) {
-	# Create IP Endpoint 
-	#$endpoint = New-Object System.Net.IPEndPoint $address, $port 
-	# Create Socket 
+[system.net.sockets.tcpclient] $global:client = $null
+[system.net.sockets.NetworkStream] $global:server_stream = $null
+$global:server_state = [hashtable]::Synchronized(@{'active'=$true})
+$global:users = [hashtable]::Synchronized(@{})
+
+
+$class_definition = @"
+using System;
+
+	public class point
+	{
+		public Int16 x;
+		public Int16 y;
+		public point(Int16 x, Int16 y)
+		{
+			this.x = x;
+			this.y = y;
+		}
+		public point()
+		{
+			this.x = 0;
+			this.y = 0;
+		}
+		public void set (Int16 x, Int16 y)
+		{
+			this.x = x;
+			this.y = y;	
+		}
+		public String toString()
+		{
+			return "(" + this.x + ", " + this.y + ")";
+		}
+	}
+
+public class User
+{
+	public byte id;
+	public String name;
+	public point position;
+
+	public User(byte id, String name)
+	{
+		this.id = id;
+		this.name = name;
+		this.position = new point();
+	}
+	public void SetPosition(Int16 x, Int16 y)
+	{
+		this.position.set(x, y);
+	}
+	public String toString()
+	{
+		return "Player (" + this.id + ")\n  " + this.name + "\n  " + this.position.toString();
+	}
+}
+"@
+Add-Type -TypeDefinition $class_definition -Language CSharp 
+Remove-Variable class_definition
+
+
+$iss = [system.management.automation.runspaces.initialsessionstate]::CreateDefault()
+$iss.ImportPSModule("PSThreading")
+$iss.Variables.Add(
+    (New-Object System.Management.Automation.Runspaces.SessionStateVariableEntry('server_state', $server_state, $null))
+)
+$rp = [runspacefactory]::CreateRunspacePool($iss)
+$rp.Open()
+
+
+
+
+
+
+
+
+
+function Connect ($username, $port, $server)
+{
 	write-host connecting to $server`:$port
 	$endpoint = new-object System.Net.IPEndPoint ([System.Net.ipaddress]::any, $null)
-	#$client = New-Object System.Net.Sockets.TcpClient $server, $port
 	$client = [net.sockets.tcpclient]$endpoint
 	
 	while (!$client.connected)
@@ -20,168 +98,209 @@ function send-msg ($message, $port, $server) {
 			$client.connect($server, $port)
 			break
 		}catch{
-			start-sleep -s 2
-			write-host "Searching for server..."
+			[console]::writeline("Searching for server at {0}:{1}", $server, $port)
+			start-sleep -s 3
 		}
 	}
 	
-	
-	$Global:server_stream = $client.GetStream()
-	$username = "user1"
+	$server_stream = $client.GetStream()
 	$message = [text.Encoding]::Ascii.GetBytes($username)
 	
-	$server_stream.write($message, 0, $message.length)
+	$server_stream.write($message, 0, [math]::min($message.length, 3))
 	$server_stream.flush()
 	
+	$success = New-Object byte[] 1
+	$count = $server_stream.Read($success, 0, 1) # Blocking
 	
-	while ($client.connected) # Detects disconnect on message send failed
-	{
-		[byte[]]$byte_stream = New-Object byte[] 200KB
-		
-		try{
-			$buff_size = $client.ReceiveBufferSize
-			write-host buff size $buff_size
-			$return = $server_stream.Read($byte_stream, 0, $buff_size)
-			If ($return -gt 0)
-			{
-				$response = [System.Text.Encoding]::ASCII.GetString($byte_stream[0..($return - 1)])
-				write-host $response
-			}
-		}catch{
-			write-host "Connection Lost..."
-			break
-		}
-
-		
-		### DEBUG ###########
-		break
-		
-		
-	}
-	
-	
-	
-	$server_stream.Dispose()
-	$client.Dispose()
-	exit
-	
+	$global:client = $client
+	$global:server_stream = $server_stream
 }
 
 
 
 
 
-$serverip = [system.net.IPAddress]::Parse($serverip) 
-send-msg "hello" $port $serverip
-
-
-
-
-
-
-<#}>
-			catch
-			{
-				#Connection to server has been closed                            
-				write-host "Connection closed..."
-				write-host $_.exception.message
-				write-host $_.exception.itemname
-				Break
-			}#>
-
-
-
-<#
-
-	while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0)
+function StartMessageReceiver ()
+{
+	[console]::writeline("!!!!!!!!!!!!!!!!!!! Starting Message Receiver")
+	$message_receive_loop =
 	{
-		$bytes[0..($i-1)]|%{$_}
-		if ($Echo)
+		param (
+			[system.net.sockets.tcpclient] $client,
+			[system.net.sockets.NetworkStream] $server_stream
+		)
+		
+		$buff_size = $client.ReceiveBufferSize
+		
+		function ProcessMessage($type)
 		{
-			$stream.Write($bytes,0,$i)
+			#while (!$server_stream.DataAvailable) {}
+			[console]::writeline("Test1")
+			#try
+			#{
+				switch($type)
+				{
+					0x01 { #state
+						[console]::writeline("Reading Type 1...")
+						$message_size_bytes = New-Object byte[] 2
+						$count = $server_stream.Read($message_size_bytes, 0, 2)
+						#$message_size = [int16]$message_size_bytes
+						#if count = 0
+						$message_size = [BitConverter]::ToInt16($message_size_bytes, 0)
+						[console]::writeline("Message Size: $message_size")
+						$user_size = 5
+						$true_message_size = $message_size * $user_size
+						
+						$message_bytes = New-Object byte[] $true_message_size
+						$count = $server_stream.Read($message_bytes, 0, $true_message_size)
+						[console]::writeline("Message: ($message_bytes)")
+
+					}
+					0x02 { #test
+						[console]::writeline("Reading Type 2...")
+						$message_size_bytes = New-Object byte[] 2
+						$count = $server_stream.Read($message_size_bytes, 0, 2)
+						#$message_size = [int16]$message_size_bytes
+						#if count = 0
+						$message_size = [BitConverter]::ToInt16($message_size_bytes, 0)
+						[console]::writeline("Message Size: $message_size")
+						
+						
+						$message_bytes = New-Object byte[] $message_size
+						$count = $server_stream.Read($message_bytes, 0, $message_size)
+						$message = [System.Text.Encoding]::ASCII.GetString($message_bytes[0..($message_size - 1)])
+						[console]::writeline("Message: ($message)")
+
+					}
+					0xff { #disconnect
+						$byte_stream = New-Object byte[] 2
+						$count = $server_stream.Read($byte_stream, 0, 2)
+						#if count = 0
+						write-host "BS: $byte_stream"
+						$x = [BitConverter]::ToInt16($byte_stream, 0)
+						write-host "Value: $x"
+						$message = [System.Text.Encoding]::ASCII.GetString($bytes[0..($return - 1)])
+
+					}
+					7 { #disconnect
+						[console]::writeline("Disconnect Received")
+						$server_state['active'] = $false
+					}
+					default {
+						[console]::writeline("Unknown message type: $type")
+						$byte_stream = New-Object byte[] $buff_size
+						$count = $server_stream.Read($byte_stream, 0, $buff_size)
+						#if count = 0
+						write-host "BS: $byte_stream"
+						$server_state['active'] = $false
+						
+					}
+				}
+
+			#} catch [Exception] {
+			#	[console]::writeline("Connection Lost (Processing type $type): `n{0}" -f $_.Exception.Message)
+			#	break
+			#}
+
+			
 		}
-	}
+		
+		
+		
+		while ($server_state['active'])
+		{
+			if ($client.Connected)
+			{
+				if ($server_stream.DataAvailable)
+				{
+					#try
+					#{
+						[console]::writeline("!!! Data Available")
+						
+						$type = New-Object byte[] 1
+						$count = $server_stream.Read($type, 0, 1) # Blocking
+						
+						
+						ProcessMessage $type
+					#}
+					#catch [Exception]
+					#{
+					#	[console]::writeline("Connection Lost (unable to get type): `n{0}" -f $_.Exception.Message)
+					#	break
+					#}
+				
+					#### DEBUG ################################
+					$server_state['active'] = $false
+				}
+			}
+			else 
+			{
+				[console]::writeline("!!! Connection Closed.")
+				Break
+			}
+			
+		}
+		
+		[console]::writeline("!!!!!!!!!!!!!!!!!!!!!!!!!!! Message Receiver Finishing")
+		
 
-
-
-
-
-
-
-
-
-
-
-	$saddrf   = [System.Net.Sockets.AddressFamily]::InterNetwork 
-	$stype    = [System.Net.Sockets.SocketType]::Dgram 
-	$ptype    = [System.Net.Sockets.ProtocolType]::UDP 
-	$sock     = New-Object System.Net.Sockets.Socket $saddrf, $stype, $ptype 
-	$sock.TTL = 26 
-	
-	$sock.bind($endpoint)
-
-	write-host Listening on port $port
-	$sock.listen()
-
-
-
-
-
-
-
-
-
-
-
-	#$client = $sock.accepttcpclient()
-
-	If ($client -ne $Null) {
-		$stream = $client.GetStream()
-		Do {
-			#Write-Host 'Processing Data'
-			Write-Verbose ("Bytes Left: {0}" -f $Client.Available)
-			$Return = $stream.Read($byte, 0, $byte.Length)
-			$String += [text.Encoding]::Ascii.GetString($byte[0..($Return-1)])
-           	} While ($stream.DataAvailable)
 	}
 	
+	$ps_message_receive = [PowerShell]::Create()
+	$ps_message_receive.RunspacePool = $rp
+	$ps_message_receive.AddScript($message_receive_loop) | out-null
+	$ps_message_receive.AddParameters(@{client=$client; server_stream=$server_stream})
 	
+	$handle_message_receive = "" | select job, shell
+	$handle_message_receive.shell = $ps_message_receive
+	$handle_message_receive.job = $ps_message_receive.BeginInvoke()
 	
+	return $handle_message_receive
+
+}
 	
-	
-	$reader = New-Object System.IO.StreamReader $stream
-	do {
-
-		$line = $reader.ReadLine()
-		write-host $line -fore cyan
-	} while ($line -and $line -ne ([char]4))
 
 
-	$reader.Dispose()
-	$stream.Dispose()
-	$client.Dispose()
-	$listener.stop()
-#>
+function Disconnect()
+{
+	[console]::writeline("Closing Client...")
+
+	if ($server_stream.Socket.Connected) { $server_stream.Close() }
+	if ($client.Connected) { $client.Close() }
+
+	$handle_message_receive.shell.EndInvoke($handle_message_receive.job)
+	$handle_message_receive.shell.Dispose()
+	$rp.Close()
+
+}
 
 
-<#
-	$saddrf   = [System.Net.Sockets.AddressFamily]::InterNetwork 
-	$stype    = [System.Net.Sockets.SocketType]::Dgram 
-	$ptype    = [System.Net.Sockets.ProtocolType]::UDP 
-	$sock     = New-Object System.Net.Sockets.Socket $saddrf, $stype, $ptype 
-	$sock.TTL = 26 
-	
-	sock.connect($endpoint)
-	
-	# Create encoded buffer 
-	$enc     = [System.Text.Encoding]::ASCII 
-	$message = "test`n"*10 
-	$buffer  = $Enc.GetBytes($Message)
-#>
+
+write-host "Sanity5"
+
+$serverip = [system.net.IPAddress]::Parse($serverip)
+
+Connect $username $port $serverip
+
+$handle_message_receive = StartMessageReceiver
 
 
 
 
+
+
+
+while ($server_state['active'])
+{
+}
+
+
+
+Disconnect
+
+
+
+[console]::writeline("END")
 
 
 
--- server.ps1
+++ server.ps1
@@ -3,162 +3,424 @@
 param(
 	[int]$port=15608
 )
-
-$Global:clients = [hashtable]::Synchronized(@{})
-$Global:client_threads = [hashtable]::Synchronized(@{})
-$Global:remove_client_queue = [System.Collections.Queue]::Synchronized((New-Object System.collections.queue))
-$Global:message_queue = [System.Collections.Queue]::Synchronized((New-Object System.collections.queue))
+Add-Type -AssemblyName System.Drawing
 
 
-$broadcast_timer = New-Object Timers.Timer
-$broadcast_timer.Enabled = $true
-$broadcast_timer.Interval = 1000 
+
+$users = [hashtable]::Synchronized(@{})
+$clients = [hashtable]::Synchronized(@{})
+$client_threads = [hashtable]::Synchronized(@{})
+$remove_client_queue = [System.Collections.Queue]::Synchronized((New-Object System.collections.queue))
+$client_message_queue = [System.Collections.Queue]::Synchronized((New-Object System.collections.queue))
+$server_state = [hashtable]::Synchronized(@{active=$true})
 
 
-function print_error($message)
-{
-	write-host -foreground red -background darkblue $message
+$class_definition = @"
+using System;
 
-}
-
-
-function create_connection ($client)
-{
-	$stream = $client.getstream()
-	$username = ""
-	[byte[]]$bytes = New-Object byte[] 5KB
-	
-	do
+	public class point
 	{
-		$return = $stream.Read($bytes, 0, $bytes.Length)
-		$username += [text.Encoding]::Ascii.GetString($bytes[0..($return-1)])
-	} while ($stream.DataAvailable)
-	
-	[console]::writeline($username)
-	# Add client
-	$clients[$username] = $client
-	
-	#Send list of online users to client
-	$broadcast_stream = $client.GetStream()
-	$broadcast_bytes = ([text.encoding]::ASCII).GetBytes("You are connected")
-	$broadcast_stream.Write($broadcast_bytes,0,$broadcast_bytes.Length)
-	$broadcast_stream.Flush()
-	
-	
-	
-	$client_runspace = [RunSpaceFactory]::CreateRunspace()
-	$client_runspace.Open()
-	$client_runspace.SessionStateProxy.setVariable("clients", $clients)
-	$client_runspace.SessionStateProxy.setVariable("client_messages", $message_queue)
-	$client_runspace.SessionStateProxy.setVariable("remove_client_queue", $remove_client_queue)
-	$client_runspace.SessionStateProxy.setVariable("username", $username)
-	$client_shell = [PowerShell]::Create()
-	$client_shell.Runspace = $client_runspace 
-	$sb =
-	{
-		#Code to kick off client connection monitor and look for incoming messages.
-		$client = $clients[$username]
-		$stream = $client.GetStream()
-		
-		#While client is connected to server, check for incoming traffic
-		While ($true) {                                              
-			[byte[]]$bytes = New-Object byte[] 200KB
-			$buff_size = $client.ReceiveBufferSize
-			$return = $stream.Read($bytes, 0, $buff_size)  
-			If ($return -gt 0)
-			{
-				$message = [System.Text.Encoding]::ASCII.GetString($bytes[0..($return - 1)])
-				$message_queue.Enqueue($message)
-			} Else {
-				$clients.Remove($username)              
-				$remove_client_queue.Enqueue($username)
-				# Removing Client
-				Break
-			}
+		public Int16 x;
+		public Int16 y;
+		public point(Int16 x, Int16 y)
+		{
+			this.x = x;
+			this.y = y;
 		}
-		
-		$client.client.shutdown([System.Net.Sockets.SocketShutdown]::Both)
-		$client.dispose()
-		$stream.dispose()
+		public point()
+		{
+			this.x = 0;
+			this.y = 0;
+		}
+		public void set (Int16 x, Int16 y)
+		{
+			this.x = x;
+			this.y = y;	
+		}
+		public String toString()
+		{
+			return "(" + this.x + ", " + this.y + ")";
+		}
+	}
+
+public class User
+{
+	private static byte next_id = 1;
+	public byte id;
+	public String name;
+	public point position;
+
+	public User(String name)
+	{
+		this.id = next_id;
+		next_id ++;
+		this.name = name;
+		this.position = new point();
+	}
+	public void SetPosition(Int16 x, Int16 y)
+	{
+		this.position.set(x, y);
+	}
+	public String toString()
+	{
+		return "Player (" + this.id + ")\n  " + this.name + "\n  " + this.position.toString();
+	}
+}
+"@
+Add-Type -TypeDefinition $class_definition -Language CSharp 
+Remove-Variable class_definition
+
+
+
+$iss = [system.management.automation.runspaces.initialsessionstate]::CreateDefault()
+$iss.ImportPSModule("PSThreading")
+$iss.Variables.Add(
+    (New-Object System.Management.Automation.Runspaces.SessionStateVariableEntry('users', $users, $null))
+)
+$iss.Variables.Add(
+    (New-Object System.Management.Automation.Runspaces.SessionStateVariableEntry('clients', $clients, $null))
+)
+$iss.Variables.Add(
+    (New-Object System.Management.Automation.Runspaces.SessionStateVariableEntry('client_threads', $client_threads, $null))
+)
+$iss.Variables.Add(
+    (New-Object System.Management.Automation.Runspaces.SessionStateVariableEntry('remove_client_queue', $remove_client_queue, $null))
+)
+$iss.Variables.Add(
+    (New-Object System.Management.Automation.Runspaces.SessionStateVariableEntry('client_message_queue', $client_message_queue, $null))
+)
+$iss.Variables.Add(
+    (New-Object System.Management.Automation.Runspaces.SessionStateVariableEntry('server_state', $server_state, $null))
+)
+
+$rp = [runspacefactory]::CreateRunspacePool($iss)
+$rp.Open()
+
+
+
+
+
+$listener_script =
+{
+	param(
+		$port
+		)
+
+	function StartMessageReceiver ($user_id)
+	{
+		[console]::writeline("!!!!!!!!!!!!!!!!!!! Starting Message Receiver for $user_id")
+		$message_receive_loop =
+		{
+			$client = $clients[$user_id]
+			$stream = $client.GetStream()
+			$buff_size = $client.ReceiveBufferSize
+			
+
+			While ($server_state['active']) {
+				[byte[]]$bytes = New-Object byte[] 200KB
+				
+				if ($client.Connected)
+				{
+					if ($stream.DataAvailable)
+					{
+						[console]::writeline("!!! Data Available")
+						$return = $stream.Read($bytes, 0, $buff_size)
+						
+						If ($return -gt 0)
+						{
+							$message = [System.Text.Encoding]::ASCII.GetString($bytes[0..($return - 1)])
+							$client_message_queue.Enqueue($message) #user_id, message
+						} Else {       
+							$remove_client_queue.Enqueue($user_id)
+							[console]::writeline("!!! Read Timeout? Thread {0} added to remove queue" -f $user_id)
+							Break
+						}
+					}
+				}
+				else 
+				{           
+					$remove_client_queue.Enqueue($user_id)
+					[console]::writeline("!!! Connection Closed. Thread {0} added to remove queue" -f $user_id)
+					Break
+				}
+			}
+			
+			[console]::writeline("!!!!!!!!!!!!!!!!!!!!!!!!!!! ($user_id) Message Receiver Finishing")
+			
+
+		}
+		$runspace_message_receive = [RunSpaceFactory]::CreateRunspace()
+		$runspace_message_receive.Open()
+		$runspace_message_receive.SessionStateProxy.setVariable("clients", $clients)
+		$runspace_message_receive.SessionStateProxy.setVariable("remove_client_queue", $remove_client_queue)
+		$runspace_message_receive.SessionStateProxy.setVariable("client_message_queue", $client_message_queue)
+		$runspace_message_receive.SessionStateProxy.setVariable("server_state", $server_state)
+		$runspace_message_receive.SessionStateProxy.setVariable("user_id", $user_id)
+		$shell_message_receive = [PowerShell]::Create()
+		$shell_message_receive.Runspace = $runspace_message_receive
+		$job = "" | select job, shell
+		$job.shell = $shell_message_receive
+		$job.job = $shell_message_receive.AddScript($message_receive_loop).BeginInvoke()
+		$client_threads[$user_id] = $job                                          
 
 	}
-	$job = "" | Select Job, PowerShell
-	$job.PowerShell = $client_shell
-	$job.job = $client_shell.AddScript($sb).BeginInvoke()
-	$client_threads[$username] = $job                                          
-
-}
 
 
+	function CreateConnection ($client)
+	{
+		$stream = $client.getstream()
+		$username = ""
+		[byte[]]$bytes = New-Object byte[] 3
 
 
+		$return = $stream.Read($bytes, 0, 3)
+		$username = [text.Encoding]::Ascii.GetString($bytes)
+		
+		[console]::writeline("New User {0}" -f $username)
+		# Add client
+		
+		#Send connection message to client
+		$stream.Write(@(0x00),0,1)
+		$stream.Flush()
+		return $username
+	}
+	
 
-
-
-
-
-
-
-
-$broadcast_timer.start()
-
-# TODO Start remove queue monitor
-
-<#
-$Global:runspace_listener = [RunSpaceFactory]::CreateRunspace()
-$runspace_listener.Open()
-$runspace_listener.SessionStateProxy.setVariable("clients", $clients)
-$runspace_listener.SessionStateProxy.setVariable("listener", $listener)
-$runspace_listener.SessionStateProxy.setVariable("port", $port)
-$Global:powershell_listener = [PowerShell]::Create()
-$powershell_listener.Runspace = $runspace_listener
-
-$listener_loop =
-{
-#>
-	[console]::writeline("$port")
+	[console]::writeline("Starting Server on port: $port")
 	$listener = [System.Net.Sockets.TcpListener]$port
-	$listener.Server.SetSocketOption("Socket", "ReuseAddress", 1)
+	#$listener.Server.SetSocketOption("Socket", "ReuseAddress", 1)
 	$listener.Start()
 	try{
 		$listener.Start()
 	}catch [Exception]{
-		print_error ("Unable to start listener: `n{0}" -f $_.Exception.Message)
+		[console]::writeline("Unable to start listener: `n{0}" -f $_.Exception.Message)
 		exit
 	}
 
 	[console]::WriteLine("{0} >> Server Started on port {1}", (Get-Date).ToString(), $port)
 
-	while ($true)
+	while ($server_state['active'])
 	{
-		[console]::writeline("Listener: {0}" -f $listener.server)
-		$client = $listener.AcceptTcpClient() # block until connection
-		If ($client -ne $Null)
+		#[console]::WriteLine("Pending: {0}" -f $listener.Pending())
+		#[console]::WriteLine("Server State: {0}" -f $server_state)
+		if ($listener.Pending())
 		{
-			create_connection $client
-		}
-		else
-		{
-			[console]::writeline("Null Client.. .")
-			break
-		}
-		
-		### DEBUG #########################
-		break
-	}
+			$client = $listener.AcceptTcpClient() # block until connection
+			[console]::writeline("Accpeted Client")
+			If ($client -ne $Null)
+			{
+				[console]::writeline("Connecting to Client.. .")
+				$username = CreateConnection $client
+				[User] $user = new-object User -ArgumentList $username
+				[console]::writeline("User ID: {0}" -f $user.id)
+				$clients[$user.id] = $client
+				$users[$user.id] = $user
 
+				[console]::writeline("Created User: `n{0}" -f $user.toString())
+				StartMessageReceiver $user.id
+			}
+			else
+			{
+				[console]::writeline("Null Client...")
+				break
+			}
+		}
+		else 
+		{
+			start-sleep -s 1
+		}
+		### DEBUG #########################
+		
+	}
+	[console]::writeline("!!!!!!!!!!!!!!!!!!!!!!!!!!!!111 Stopping Listener")
 
 	$listener.stop()
-	exit
 
-#}
+}
+
+#$remove_client_queue.enqueue("toast")
+[console]::writeline("Sanity3")
 
 
-#$Global:handle = $powershell_listener.AddScript($listener_loop).BeginInvoke()
+
+$ps_listener = [PowerShell]::Create()
+$ps_listener.RunspacePool = $rp
+$ps_listener.AddScript($listener_script) | out-null
+$ps_listener.AddParameters(@{port=$port}) | out-null
+
+$handle_listener = "" | select job, shell
+$handle_listener.shell = $ps_listener
+$handle_listener.job = $ps_listener.BeginInvoke()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function RemoveClient($user_id)
+{
+	[console]::writeline("Removing client: $user_id")
+	$client_threads[$user_id].shell.EndInvoke($client_threads[$user_id].job)
+    $client_threads[$user_id].shell.Runspace.Close()
+    $client_threads[$user_id].shell.Dispose()
+    $client_threads.Remove($user_id)
+
+	if ($clients[$user_id].Connected)
+	{
+		if ($clients[$user_id].GetStream().Socket.Connected) { $clients[$user_id].GetStream().Close() }
+		$clients[$user_id].Client.Shutdown([System.Net.Sockets.SocketShutdown]::Both)
+	}
+	$clients[$user_id].Close()
+	$clients.Remove($user_id)
+
+
+	$users.Remove($user_id)
+	[console]::writeline("!!! [{0}] removed" -f $user_id)
+
+}
+
+
+function CheckRemoveClients()
+{
+	while ($remove_client_queue.Count -ne 0)
+	{
+		RemoveClient $remove_client_queue.Dequeue()
+		[console]::writeline("Removing client... Done")
+	}
+}
+
+
+function RemoveAllClients()
+{
+	[console]::writeline("Remove All CLients")
+
+	foreach($key in $($clients.keys))
+	{
+		RemoveClient $key
+	}
+}
+
+
+function BroadcastMessage ($message)
+{
+	CheckRemoveClients
 	
+	
+	foreach ($user_id in $clients.keys)
+	{
+		$client = $clients[$user_id]
+
+		if (-not $client.connected)
+		{
+			$remove_client_queue.Enqueue($user_id)
+			[console]::writeline("$client dead: $user_id")
+			continue
+		}
+
+		write-host "$user_id -> $message"
+		$broadcast_stream = $client.GetStream()
+		#$broadcast_bytes = ([text.encoding]::ASCII).GetBytes($message)
+		
+		#try{
+			[console]::writeline("count: {0}" -f $users.count)
+			[int16]$size = [int16]$users.count
+			[console]::writeline("count: {0}" -f $size)
+			$true_size = 3 + ($users.count) * 5
+			[console]::writeline("count: {0}" -f $true_size)
+			$byte_size = [BitConverter]::GetBytes($size)
+			$bytes = new-object byte[] $true_size
+			$bytes[0] = 0x01
+			$bytes[1] = $byte_size[0]
+			$bytes[2] = $byte_size[1]
+			$i = 3
+			foreach ($uid in $users.keys)
+			{
+				#if ($uid -eq $user_id) { continue }
+				$bytes[$i++] = $uid
+				$x = [BitConverter]::GetBytes($users[$uid].position.x)
+				$y = [BitConverter]::GetBytes($users[$uid].position.x)
+				$bytes[$i++] = $x[0]
+				$bytes[$i++] = $x[1]
+				$bytes[$i++] = $y[0]
+				$bytes[$i++] = $y[1]
+
+			}
+			[console]::writeline("Message Bytes: $bytes")
+			$broadcast_stream.Write($bytes,0,$true_size)
+			$broadcast_stream.Flush()
+			[console]::writeline("--- Message Sent")
+			continue
 
 
-start-sleep -s 10
+			[byte] $type = 0x01
+			[console]::writeline("Type: $type")
+			
+			$message_bytes = ([text.encoding]::ASCII).GetBytes($message)
+			[int16]$message_size = [int16]$message_bytes.Count
+			$message_size_bytes = [BitConverter]::GetBytes($message_size)
+			
+			[console]::writeline("Message_size: $message_size")
+			[console]::writeline("messgae_bytes: $message_bytes")
+			
+			$broadcast_stream.Write($type,0,1)
+			$broadcast_stream.Write($message_size_bytes,0,2)
+			$broadcast_stream.Write($message_bytes,0,$message_size)
+			
+		#} catch [Exception]{
+		#	[console]::writeline("Unable to write to {0}: `n{1}", $user, $_.Exception.Message)
+		#	$remove_client_queue.Enqueue($user_id)
+		#}
+	}
+	
+}
+
+
+
+
+
+while ($clients.count -eq 0) {}
+
+$i = 0
+while ($true)
+{
+	write-host "Keys: " + $clients.keys
+	foreach ($u in $clients.keys)
+	{
+		write-host $u
+	}
+	start-sleep -s 1
+	BroadcastMessage "Message $i"
+	$i++
+	
+	if ($i -gt 2)
+	{
+		$server_state['active'] = $false
+		start-sleep -s 1
+		BroadcastMessage "~~Disconnect"
+		start-sleep -s 1
+		RemoveAllCLients
+
+		break
+	}
+}
+
+
+$handle_listener.shell.EndInvoke($handle_listener.job)
+$handle_listener.shell.Dispose()
+$rp.Close()
+[console]::writeline("END")
 
 
 
@@ -169,18 +431,47 @@ start-sleep -s 10
 
 
 
+<# Send Message
+			[byte] $type = 0x01
+			[console]::writeline("Type: $type")
+			
+			$message_bytes = ([text.encoding]::ASCII).GetBytes($message)
+			[int16]$message_size = [int16]$message_bytes.Count
+			$message_size_bytes = [BitConverter]::GetBytes($message_size)
+			
+			[console]::writeline("Message_size: $message_size")
+			[console]::writeline("messgae_bytes: $message_bytes")
+			
+			$broadcast_stream.Write($type,0,1)
+			$broadcast_stream.Write($message_size_bytes,0,2)
+			$broadcast_stream.Write($message_bytes,0,$message_size)
+			
+			$broadcast_stream.Flush()
+			[console]::writeline("--- Message Sent")
+#>
+
+<# Mass Read
+do
+{
+	$return = $stream.Read($bytes, 0, $bytes.length)
+	$message += [text.Encoding]::Ascii.GetString($bytes[0..($return-1)])
+} while ($stream.DataAvailable)
+#>
 
 
 
 
 
+<#
+$broadcast_timer = New-Object Timers.Timer
+$broadcast_timer.Enabled = $true
+$broadcast_timer.Interval = 1000 
 
 
 
 
-
-
-
+$broadcast_timer.start()
+#>
 
 
 
