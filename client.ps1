f828b56d6ec5b1f39b15e336d9304f6f84e49927
reallyspeed <david@reallyspeed.com>
2017-05-19 09:26:51 -0500

    Add files via upload

--- client.ps1
+++ client.ps1
@@ -1,4 +1,4 @@
-[CmdletBinding()]
+﻿[CmdletBinding()]
 #Param(
 #$Command = $(Read-Host "Enter the script file"), 
 #[Parameter(ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)]$ObjectList
@@ -13,42 +13,42 @@ param(
 $class_definition = @"
 using System;
 
-	public class point
+public class point
+{
+	public Int16 x;
+	public Int16 y;
+	public point(Int16 x, Int16 y)
 	{
-		public Int16 x;
-		public Int16 y;
-		public point(Int16 x, Int16 y)
-		{
-			this.x = x;
-			this.y = y;
-		}
-		public point()
-		{
-			this.x = 0;
-			this.y = 0;
-		}
-		public void set (Int16 x, Int16 y)
-		{
-			this.x = x;
-			this.y = y;	
-		}
-		public void set (byte[] bytes)
-		{
-			this.x = BitConverter.ToInt16(bytes, 0);
-			this.y = BitConverter.ToInt16(bytes, 2);			
-		}
-		public byte[] bytes()
-		{
-			byte[] r = new byte[4];
-			BitConverter.GetBytes(this.x).CopyTo(r, 0);
-			BitConverter.GetBytes(this.y).CopyTo(r, 2);
-			return r;
-		}
-		public String toString()
-		{
-			return "(" + this.x + ", " + this.y + ")";
-		}
+		this.x = x;
+		this.y = y;
 	}
+	public point()
+	{
+		this.x = 0;
+		this.y = 0;
+	}
+	public void set (Int16 x, Int16 y)
+	{
+		this.x = x;
+		this.y = y;	
+	}
+	public void set (byte[] bytes)
+	{
+		this.x = BitConverter.ToInt16(bytes, 0);
+		this.y = BitConverter.ToInt16(bytes, 2);			
+	}
+	public byte[] bytes()
+	{
+		byte[] r = new byte[4];
+		BitConverter.GetBytes(this.x).CopyTo(r, 0);
+		BitConverter.GetBytes(this.y).CopyTo(r, 2);
+		return r;
+	}
+	public String toString()
+	{
+		return "(" + this.x + ", " + this.y + ")";
+	}
+}
 
 public class User
 {
--- server.ps1
+++ server.ps1
@@ -1,4 +1,4 @@
-[CmdletBinding()]
+﻿[CmdletBinding()]
 
 param(
 	[int]$port=15608
@@ -18,42 +18,42 @@ $server_state = [hashtable]::Synchronized(@{active=$true})
 $class_definition = @"
 using System;
 
-	public class point
+public class point
+{
+	public Int16 x;
+	public Int16 y;
+	public point(Int16 x, Int16 y)
 	{
-		public Int16 x;
-		public Int16 y;
-		public point(Int16 x, Int16 y)
-		{
-			this.x = x;
-			this.y = y;
-		}
-		public point()
-		{
-			this.x = 0;
-			this.y = 0;
-		}
-		public void set (Int16 x, Int16 y)
-		{
-			this.x = x;
-			this.y = y;	
-		}
-		public void set (byte[] bytes)
-		{
-			this.x = BitConverter.ToInt16(bytes, 0);
-			this.y = BitConverter.ToInt16(bytes, 2);			
-		}
-		public byte[] bytes()
-		{
-			byte[] r = new byte[4];
-			BitConverter.GetBytes(this.x).CopyTo(r, 0);
-			BitConverter.GetBytes(this.y).CopyTo(r, 2);
-			return r;
-		}
-		public String toString()
-		{
-			return "(" + this.x + ", " + this.y + ")";
-		}
+		this.x = x;
+		this.y = y;
 	}
+	public point()
+	{
+		this.x = 0;
+		this.y = 0;
+	}
+	public void set (Int16 x, Int16 y)
+	{
+		this.x = x;
+		this.y = y;	
+	}
+	public void set (byte[] bytes)
+	{
+		this.x = BitConverter.ToInt16(bytes, 0);
+		this.y = BitConverter.ToInt16(bytes, 2);			
+	}
+	public byte[] bytes()
+	{
+		byte[] r = new byte[4];
+		BitConverter.GetBytes(this.x).CopyTo(r, 0);
+		BitConverter.GetBytes(this.y).CopyTo(r, 2);
+		return r;
+	}
+	public String toString()
+	{
+		return "(" + this.x + ", " + this.y + ")";
+	}
+}
 
 public class User
 {
